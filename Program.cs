28f76ee60236afbab260fde9c56fe26b2e6c314e
Jack Doe <jackdoe@Jacks-MacBook-Pro-47.local>
2024-09-24 14:37:26 +1200

    fixed the loop in Jack()

--- Program.cs
+++ Program.cs
@@ -45,28 +45,36 @@ namespace curly_octo_potato_code
                 {
                     case "monday":
                         Console.WriteLine("mondays are literally the worst.You are insane");
+                        validAnswer = true;
                         break;
                     case "tuesday":
                         Console.WriteLine("Tuesdays are just Mondays without the Monday");
+                        validAnswer = true;
                         break;
                     case "wednesday":
                         Console.WriteLine("Wednesday is good I guess");
+                        validAnswer = true;
                         break;
                     case "thursday":
                         Console.WriteLine("Thursdays are good. Friday is close, and Monday is far");
+                        validAnswer = true;
                         break;
                     case "friday":
                         Console.WriteLine("Finally, the weekend! ");
+                        validAnswer = true;
                         break;
                     case "saturday":
                         Console.WriteLine("Saturdays are good. I like to do nothing.");
+                        validAnswer = true;
                         break;
                     case "sunday":
                         Console.WriteLine("I also like Sundays, but I don't like how close Monday is");
+                        validAnswer = true;
                         break;
                     default:
                         Console.WriteLine("That's not a valid day of the week.");
                         break;
+                        Thread.Sleep(5000);
                 }
             }
         }

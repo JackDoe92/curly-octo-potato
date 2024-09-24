12f9704b6f48a9523d88b3c60669d15a4006a64b
Jack Doe <jackdoe@Jacks-MacBook-Pro-47.local>
2024-09-24 13:42:51 +1200

    Made the program

---
+++ curly_octo_potato_code/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace curly_octo_potato_code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+
+        }
+    }
+}

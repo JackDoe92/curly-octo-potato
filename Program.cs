5d2ac86fac0da66ed66e568abba5998a30a20ef4
Samuel Pearson (1000131065) <PEARSN2@student.op.ac.nz>
2024-09-24 14:14:14 +1200

    Updated code

--- Program.cs
+++ Program.cs
@@ -1,12 +1,50 @@
-﻿namespace curly_octo_potato_code
+﻿using System.Reflection.Metadata;
+
+namespace curly_octo_potato_code
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.ReadLine();
+            Console.WriteLine("Hi, what's your name?");
+            string name = Convert.ToString(Console.ReadLine().ToLower());
+            if (name == "jack")
+            {
+                Console.Clear();
+                Jack();
+            }
+            else
+            {
+                if (name == "jacob")
+                {
+                    Console.Clear();
+                    Jacob();
+                }
+                else
+                    if (name == "xavier")
+                {
+                    Console.Clear();
+                    Xavier();
+                }
+            }    
 
+        }
+        public static void Xavier()
+        {
+            Console.WriteLine("g'day");
+            Console.ReadLine();
+        }                
+                                                
+        public static void Jacob()
+        {
+            Console.WriteLine("tes2");
+            Console.ReadLine();
+        }       
+               
+        public static void Jack()
+        {
+            Console.WriteLine("test3");
+            Console.ReadLine();
         }
     }
 }
